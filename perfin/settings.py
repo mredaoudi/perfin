81c51e631deb58a205b56ed0982338b3f69ee04e
Mohammed-Reda Daoudi <daoudi.mreda@outlook.be>
2024-05-27 16:17:22 +0200

    fix: deploy settings

--- perfin/settings.py
+++ perfin/settings.py
@@ -131,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'landing'
+
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
